cd1d5831df08af27dc8e4c0c7d318f62cf6fc8dc
Hannah Kingsland <hannah_kingsland@hotmail.co.uk>
2017-10-14 15:48:25 +0100

    Worked on Maps Activity java code, trying to gather current location

--- app/src/main/java/com/example/hannah/songle/MainActivity.java
+++ app/src/main/java/com/example/hannah/songle/MainActivity.java
@@ -5,14 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) (findViewById(R.id.button2));
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button was clicked");
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,5 +37,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
+
