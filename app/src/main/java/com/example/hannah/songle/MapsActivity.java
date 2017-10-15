f3bde30f9d75af5c81289be75e5ffac7a9a23d83
Hannah Kingsland <hannah_kingsland@hotmail.co.uk>
2017-10-15 15:43:37 +0100

    Added functionality to MapsActivity

--- app/src/main/java/com/example/hannah/songle/MapsActivity.java
+++ app/src/main/java/com/example/hannah/songle/MapsActivity.java
@@ -1,8 +1,16 @@
 package com.example.hannah.songle;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +18,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean mLocationPermissionGranted = false;
+    private static final String TAG = "MapsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +37,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    protected void createLocationRequest() {
+        //Set the parameters for the location request
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000); //preferably every 5 seconds
+        mLocationRequest.setFastestInterval(1000); //at most every second
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        //Can we access the user's current location?
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+    }
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        try { createLocationRequest(); }
+        catch (java.lang.IllegalStateException ise) {
+            System.out.println("IllegalStateException thrown [onConnected");
+        }
+        //Can we access the user's current location?
+        if (ContextCompact.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location current) {
+        System.out.println(
+                "[onLocationChanged] Lat/long now (" +
+                        String.valueOf(current.getLatitude()) + "," +
+                        String.valueOf(current.getLongitude()) + ")"
+        );
+        //Do something with current location
+    }
+
+    @Override
+    public void onConnectionSuspended(int flag) {
+        System.out.println(" >>>> onConnectionSuspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        //An unresolvable error has occurred and a connection to Google APIs
+        // could not be established. Display an error message, or handle the failure silently
+        System.out.println(" >>>> onConnectionFailed");
+    }
 
     /**
      * Manipulates the map once available.
