9676766297e6456ece11e8e8462d716dcee63651
Hannah Kingsland <s1518196@ryding.inf.ed.ac.uk>
2017-10-25 16:42:11 +0100

    Worked on XmlParser

--- app/src/main/java/com/example/hannah/songle/NetworkActivity.java
+++ app/src/main/java/com/example/hannah/songle/NetworkActivity.java
@@ -1,15 +1,55 @@
 package com.example.hannah.songle;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.net.URL;
 
 public class NetworkActivity extends FragmentActivity implements DownloadCallback {
     private NetworkFragment mNetworkFragment;
     private boolean mDownloading = false;
+
+        public static final String WIFI = "Wi-Fi";
+        public static final String ANY = "Any";
+        private static final String URL = "http://www.inf.ed.ac.uk/teaching/courses/selp/data/songs/songs.xml";
+
+        // Whether there is a Wi-Fi connection.
+        private static boolean wifiConnected = false;
+        // Whether there is a mobile connection.
+        private static boolean mobileConnected = false;
+        // Whether the display should be refreshed.
+        public static boolean refreshDisplay = true;
+        public static String sPref = null;
+
+        // Uses AsyncTask to download the XML feed from the webpage.
+        public void loadPage() {
+
+            if((sPref.equals(ANY)) && (wifiConnected || mobileConnected)) {
+                new DownloadXmlTask().execute(URL);
+            }
+            else if ((sPref.equals(WIFI)) && (wifiConnected)) {
+                new DownloadXmlTask().execute(URL);
+            } else {
+                // show error
+            }
+        }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,5 +107,88 @@ public class NetworkActivity extends FragmentActivity implements DownloadCallbac
         }
     }
 
+
+    // Implementation of AsyncTask used to download XML feed from stackoverflow.com.
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            setContentView(R.layout.activity_main);
+            // Displays the HTML string in the UI via a WebView
+            WebView myWebView = (WebView) findViewById(R.id.webview);
+            myWebView.loadData(result, "text/html", null);
+        }
+    }
+    // Uploads XML from stackoverflow.com, parses it, and combines it with
+// HTML markup. Returns HTML string.
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        // Instantiate the parser
+        XmlParser stackOverflowXmlParser = new XmlParser();
+        List<XmlParser.Entry> entries = null;
+        int number = -1;
+        String artist = null;
+        String title = null;
+        String url = null;
+        Calendar rightNow = Calendar.getInstance();
+        DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+        // Checks whether the user set the preference to include summary text
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean pref = sharedPrefs.getBoolean("summaryPref", false);
+
+        StringBuilder htmlString = new StringBuilder();
+        htmlString.append("<h3>" + getResources().getString(R.string.page_title) + "</h3>");
+        htmlString.append("<em>" + getResources().getString(R.string.updated) + " " +
+                formatter.format(rightNow.getTime()) + "</em>");
+
+        try {
+            stream = downloadUrl(urlString);
+            entries = XmlParser.parse(stream);
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
+        // Each Entry object represents a single post in the XML feed.
+        // This section processes the entries list to combine each entry with HTML markup.
+        // Each entry is displayed in the UI as a link that optionally includes
+        // a text summary.
+        for (XmlParser.Entry entry : entries) {
+            htmlString.append("<p><a href='");
+            htmlString.append(entry.link);
+            htmlString.append("'>" + entry.title + "</a></p>");
+            htmlString.append(entry.artist);
+        }
+        return htmlString.toString();
+    }
+
+    // Given a string representation of a URL, sets up a connection and gets
+// an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        return conn.getInputStream();
+    }
 
 }
--- app/src/main/java/com/example/hannah/songle/XmlParser.java
+++ app/src/main/java/com/example/hannah/songle/XmlParser.java
@@ -7,6 +7,7 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,7 +17,7 @@ import java.util.List;
 public class XmlParser {
     private static final String ns = null;
 
-    public List parse(InputStream in) throws XmlPullParserException, IOException {
+    public static List parse(InputStream in) throws XmlPullParserException, IOException {
         try {
             XmlPullParser parser = Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
@@ -27,5 +28,135 @@ public class XmlParser {
             in.close();
         }
     }
+
+    private static List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("entry")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    public static class Entry {
+        public final String title;
+        public final String link;
+        public final int number;
+        public final String artist;
+
+        private Entry(int number, String title, String artist, String link) {
+            this.number = number;
+            this.title = title;
+            this.artist = artist;
+            this.link = link;
+        }
+    }
+
+    // Parses the contents of an entry. If it encounters a title, summary, or link tag, hands them off
+// to their respective "read" methods for processing. Otherwise, skips the tag.
+    private static Entry readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "entry");
+        int number = -1;
+        String title = null;
+        String artist = null;
+        String link = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("title")) {
+                title = readTitle(parser);
+            } else if (name.equals("artist")) {
+                artist = readArtist(parser);
+            } else if (name.equals("link")) {
+                link = readLink(parser);
+            } else if (name.equals("number")){
+                number = readNumber(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new Entry(number, title, artist, link);
+    }
+
+    private static int readNumber(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "number");
+        int number = Integer.parseInt(readText(parser));
+        parser.require(XmlPullParser.END_TAG, ns, "number");
+        return number;
+    }
+
+    // Processes title tags in the feed.
+    private static String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "title");
+        return title;
+    }
+
+    // Processes link tags in the feed.
+    private static String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String link = "";
+        parser.require(XmlPullParser.START_TAG, ns, "link");
+        String tag = parser.getName();
+        String relType = parser.getAttributeValue(null, "rel");
+        if (tag.equals("link")) {
+            if (relType.equals("alternate")) {
+                link = parser.getAttributeValue(null, "href");
+                parser.nextTag();
+            }
+        }
+        parser.require(XmlPullParser.END_TAG, ns, "link");
+        return link;
+    }
+
+    // Processes artist tags in the feed.
+    private static String readArtist(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "artist");
+        String artist = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "artist");
+        return artist;
+    }
+
+    // For the tags title and artist, extracts their text values.
+    private static String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+    private static void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+
+
 
 }
